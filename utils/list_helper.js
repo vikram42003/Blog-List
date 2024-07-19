c8b7045af90c4ba8020ba7c13539168d1af71c4d
vikram(wsl) <vikramjit360@gmail.com>
2024-07-19 20:27:25 +0530

    4.5 - add favouriteBlog function and corresponding tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = blogs => {
   return blogs ? blogs.reduce((acc, curr) => (acc += curr.likes), 0) : 0;
 };
 
+const favouriteBlog = blogs => {
+  if (!blogs) return null;
+
+  let fav = blogs[0];
+  blogs.forEach(blog => {
+    if (blog.likes > fav.likes) fav = blog;
+  });
+
+  return {
+    title: fav.title,
+    author: fav.author,
+    likes: fav.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
