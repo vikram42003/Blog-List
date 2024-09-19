450c2b19f3510d3a3d664886f20886f02872a168
vikram(wsl) <vikramjit360@gmail.com>
2024-09-19 14:51:44 +0530

    7.18 - feat, bugfix: add a comments array to blogs and POST route for comments
    - minor bugfix in middleware.js which was sending 404 on ValidationError

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,11 +3,13 @@ const Blog = require("../models/blog");
 const User = require("../models/user");
 const userExtractor = require("../utils/userExtractor");
 
+// GET - "/api/blogs/" - get all blogs
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { blogs: 0 });
   response.json(blogs);
 });
 
+// POST - "/api/blogs/" - create a new blog
 blogsRouter.post("/", userExtractor, async (request, response) => {
   const blog = new Blog(request.body);
   const user = request.user;
@@ -21,6 +23,29 @@ blogsRouter.post("/", userExtractor, async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
+// POST - "/api/blogs/:id/comments" - add a comment to the blog
+blogsRouter.post("/:id/comments", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+
+  if (!request.body.comment) {
+    let error = new Error("cannot add an empty comment");
+    error.name = "MissingData";
+    throw error;
+  }
+  if (!blog) {
+    let error = new Error(`the blog with id - ${request.params.id} was not found on the server`);
+    error.name = "MissingData";
+    throw error;
+  }
+
+  blog.comments = blog.comments.concat(request.body.comment);
+
+  await blog.save();
+
+  response.status(201).json(request.body.comment);
+});
+
+// DELETE - "/api/blogs/:id" - delete the blog
 blogsRouter.delete("/:id", userExtractor, async (request, response) => {
   const user = request.user;
   const blog = await Blog.findById(request.params.id);
@@ -50,6 +75,7 @@ blogsRouter.delete("/:id", userExtractor, async (request, response) => {
   }
 });
 
+// PUT - "/api/blogs/:id" - update likes on the blog
 blogsRouter.put("/:id", userExtractor, async (request, response) => {
   const newBlog = request.body;
   newBlog.user = newBlog.user ? newBlog.user.id : null;
--- utils/middleware.js
+++ utils/middleware.js
@@ -14,7 +14,7 @@ const errorHandler = (error, request, response, next) => {
   } else if (error.name === "NotAuthorized") {
     return response.status(401).json({ error: error.message });
   } else if (error.name === "MissingData") {
-    return response.status(404).json({ error: error.message });
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
